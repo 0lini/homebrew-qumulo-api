8a7e361304892b46b8af7159e6d646df46499a1f
0lini <72492483+0lini@users.noreply.github.com>
2026-01-15 11:33:56 +0100

    Update license field in qumulo-api.rb

--- Formula/qumulo-api.rb
+++ Formula/qumulo-api.rb
@@ -5,7 +5,7 @@ class QumuloApi < Formula
   homepage "https://pypi.org/project/qumulo-api/"
   url "https://files.pythonhosted.org/packages/d3/43/cb66667f075c7d851ee38f199ca2b62dabc5d9184102a13b23a332d40da1/qumulo_api-7.7.3-py3-none-any.whl"
   sha256 "eab9b488800a3bd82a862364bb1e98331601d8c88c1d3982be47bcdae9c3e64d"
-  license "Apache-2.0"
+  license :cannot_represent
 
   livecheck do
     url "https://pypi.org/pypi/qumulo-api/json"
