7bb331b4cc233d14411f5b790cbe32805134cc43
0lini <72492483+0lini@users.noreply.github.com>
2026-01-15 11:44:46 +0100

    Revert: Refactor install method to use virtualenv_install_with_resources
    Issues with installation since https://github.com/0lini/homebrew-qumulo-api/commit/5a1a2119e3adc790b58822473811a8293fad73e4

--- Formula/qumulo-api.rb
+++ Formula/qumulo-api.rb
@@ -67,7 +67,10 @@ class QumuloApi < Formula
   end
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resources
+    whl = buildpath.glob("qumulo_api-7.7.3-py3-none-any.whl").first
+    venv.pip_install_and_link whl
   end
 
   test do
